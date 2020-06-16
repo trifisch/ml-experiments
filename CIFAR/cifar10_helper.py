cf688b6d51089f3f45a177e3014163832da214bb
Trifisch <trifisch@gmail.com>
2020-06-16 15:45:10 +0200

    Automatic creation of parameters

--- CIFAR/cifar10_helper.py
+++ CIFAR/cifar10_helper.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import numpy as np
 from tensorflow.python.keras.callbacks import TensorBoard
 from tensorflow.python.keras.engine.input_layer import Input
 from tensorflow.python.keras.layers.convolutional import Conv2D
@@ -7,7 +8,10 @@ from tensorflow.python.keras.layers.core import Activation, Dense, Flatten
 from tensorflow.python.keras.layers.pooling import MaxPool2D
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.layers.normalization import BatchNormalization
+from tensorflow.keras.optimizers import *
 from matplotlib import pyplot as plt
+
+PARAM_FILE = 'params.txt'
 
 # Also log the learning rate in TensorBoard
 class LRTensorBoard(TensorBoard):
@@ -41,6 +45,90 @@ def get_model_save_path():
         model_dir, "model_" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
     
     return model_path
+
+# save given parameters to file
+def save_params(path, params, header):
+    out_str = header + "\n"
+    out_str += '"name": "' + params["name"] + "\n"
+    out_str += '"epochs": ' + str(params["epochs"]) + '"\n'
+    out_str += '"batch_size": "' + str(params["batch_size"]) + '"\n'
+    out_str += '"model":\n'
+
+    model_params = params["model"]
+    out_str += '"optimizer": "' + str(model_params["optimizer"]) + '"\n'
+    out_str += '"learning_rate": ' + str(model_params["learning_rate"]) + '\n'
+    
+    out_str += '"network":\n'
+    for element in model_params["network"]:
+        if element[0] == "C2D":
+            out_str += str(element[0]) + ", " + str(element[1]) + ", " + str(element[2]) + ", " + str(element[3]) + "\n"
+        elif element[0] == "Dense":
+            out_str += str(element[0]) + ", " + str(element[1]) + "\n"
+        elif element[0] == "A":
+            out_str += str(element[0]) + ", " + str(element[1]) + "\n"
+        elif element[0] == "MaxPool2D":
+            out_str += str(element[0]) + "\n"
+        elif element[0] == "Flatten":
+            out_str += str(element[0]) + "\n"
+        else:
+            out_str += "Invalid element\n"
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+    output_file = os.path.join(path, PARAM_FILE)
+
+    with open(output_file, 'w') as output:
+        output.write(out_str)  # save as text
+        output.close()
+
+
+# create random parameters
+def create_params(number):
+    params = []
+
+    for i in range(number):
+        param = {}
+        param["name"] = str(i+1)
+        param["epochs"] = 30
+        param["batch_size"] = 128
+        
+        model = {}
+        model["optimizer"] = Adam
+        model["learning_rate"] = np.random.choice([1e-2, 1e-3, 1e-4, 1e-5, 1e-6])
+
+        # create random network
+        network = [] 
+        nr_conv_layers = np.random.choice([1, 2, 3, 4, 5])
+        double_layers = np.random.choice([True, False])
+
+        for j in range(nr_conv_layers):
+            filters = np.random.choice([64, 128, 256, 512, 1024])
+            kernel_size = np.random.choice([3, 5, 7, 9])
+            batch_norm = np.random.choice([True, False])
+
+            conv_layer = ["C2D", filters, (kernel_size, kernel_size), batch_norm]
+            network.append(conv_layer)
+            network.append(["A", "relu"])
+
+            if double_layers:
+                network.append(conv_layer)
+                network.append(["A", "relu"])
+            
+            network.append(["MaxPool2D"])
+
+        # ["Flatten"], ["Dense", 140], ["A", "relu"]
+        network.append(["Flatten"])
+        dense_size = np.random.choice([64, 128, 256, 512, 1024])
+        network.append(["Dense", dense_size])
+        network.append(["A", "relu"])
+
+        model["network"] = network
+        param["model"] = model
+        
+        params.append(param)
+    
+    return params
+
 
 
 # *** The Model Function ***
--- CIFAR/cifar10_model_search.py
+++ CIFAR/cifar10_model_search.py
@@ -8,7 +8,7 @@ from tensorflow.python.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 from cifar10_data import CIFAR10
 from cifar10_params import nn_params
-from cifar10_helper import get_tensorboard_callback, get_model_save_path, get_model, plot_history
+from cifar10_helper import get_tensorboard_callback, get_model_save_path, save_params, get_model, plot_history, create_params
 
 
 # reduce info messages and warnings from TF
@@ -19,6 +19,10 @@ tf.autograph.set_verbosity(0)
 # (scale_mode: "standard", "minmax", "none"; augment_size: set to 0 to skip augmentation)
 data = CIFAR10(scale_mode="minmax", augment_size=5000)
 
+# auto create parameters
+# => comment out if you want to use the parameters from the parameter file!
+nn_params = create_params(10)
+
 # Iterate through parameter settings
 best_val_accuracy=0.0
 count = 0
@@ -27,6 +31,7 @@ max_count = len(nn_params)
 for params in nn_params:
     count += 1
     print(f"------ Parameter set {count} of {max_count} ------")
+    print(str(params) + "\n")
 
     # Build the model
     model = get_model(data, params["model"])
@@ -50,9 +55,13 @@ for params in nn_params:
     print(f"*** Model (params[{count-1}] >{params['name']}<): accuracy={history.history['accuracy'][-1]:.4f}, val_accuracy={val_accuracy:.4f}, ")
 
     if val_accuracy > best_val_accuracy:
-        # new best model found => print message and save model
+        # new best model found => print message, save model and its paramaters
         print(f"****** New best model! ******")
-        model.save(get_model_save_path())
+        path = get_model_save_path()
+        model.save(path)
+        header = f"*** Model (params[{count-1}] >{params['name']}<): accuracy={history.history['accuracy'][-1]:.4f}, val_accuracy={val_accuracy:.4f}, "
+        save_params(path, params, header)
+
         best_val_accuracy = val_accuracy
         # visualize learning progress in (so far) best model
         #plot_history(history)
